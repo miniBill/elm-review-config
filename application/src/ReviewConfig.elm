247dcca689238b66994bb1128c7ab6659953d635
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-01-28 11:07:17 +0100

    Add my preferred config

--- application/src/ReviewConfig.elm
+++ application/src/ReviewConfig.elm
@@ -11,17 +11,37 @@ when inside the directory containing this file.
 
 -}
 
+import Derive
 import Docs.ReviewAtDocs
+import Docs.ReviewLinksAndSections
+import EqualsCaseable
+import LimitAliasedRecordSize
+import NoBooleanCase
+import NoBrokenParserFunctions
+import NoCatchAllForSpecificRemainingPatterns
 import NoConfusingPrefixOperator
 import NoDebug.Log
 import NoDebug.TodoOrToString
+import NoDeprecated
+import NoDuplicatePorts
 import NoExposingEverything
 import NoImportingEverything
+import NoInternalImports
+import NoInvalidRGBValues
+import NoMissingSubscriptionsCall
 import NoMissingTypeAnnotation
 import NoMissingTypeAnnotationInLetIn
 import NoMissingTypeExpose
+import NoModuleOnExposedNames
 import NoPrematureLetComputation
+import NoRecursiveUpdate
+import NoRedundantlyQualifiedType
 import NoSimpleLetBody
+import NoSinglePatternCase
+import NoUnnecessaryTrailingUnderscore
+import NoUnoptimizedRecursion
+import NoUnsafeDivision
+import NoUnsafePorts
 import NoUnused.CustomTypeConstructorArgs
 import NoUnused.CustomTypeConstructors
 import NoUnused.Dependencies
@@ -29,30 +49,75 @@ import NoUnused.Exports
 import NoUnused.Parameters
 import NoUnused.Patterns
 import NoUnused.Variables
+import Review.Documentation.CodeSnippet
 import Review.Rule as Rule exposing (Rule)
+import ReviewPipelineStyles
+import ReviewPipelineStyles.Fixes
 import Simplify
+import UseCamelCase
+import Validate.Regexes
+import VariablesBetweenCaseOf.AccessInCases
 
 
 config : List Rule
 config =
     [ Docs.ReviewAtDocs.rule
+    , Derive.rule True []
+    , Docs.ReviewLinksAndSections.rule
+    , EqualsCaseable.forbid EqualsCaseable.Everywhere
+    , HtmlToElm.rule
+    , LimitAliasedRecordSize.rule (20 |> LimitAliasedRecordSize.maxRecordSize)
+    , NoBooleanCase.rule
+    , NoBrokenParserFunctions.rule
+    , NoCatchAllForSpecificRemainingPatterns.rule { onlyReportCatchAllIfEquivalentToSinglePattern = False }
     , NoConfusingPrefixOperator.rule
-    , NoDebug.Log.rule
-    , NoDebug.TodoOrToString.rule
-        |> Rule.ignoreErrorsForDirectories [ "tests/" ]
+    , NoDebug.Log.rule |> Rule.ignoreErrorsForDirectories [ "tests/" ]
+    , NoDebug.TodoOrToString.rule |> Rule.ignoreErrorsForDirectories [ "tests/" ]
+    , NoDeprecated.rule NoDeprecated.defaults
+    , NoDuplicatePorts.rule
     , NoExposingEverything.rule
     , NoImportingEverything.rule []
+    , NoInternalImports.rule []
+    , NoInvalidRGBValues.rule
+    , NoMissingSubscriptionsCall.rule
     , NoMissingTypeAnnotation.rule
     , NoMissingTypeAnnotationInLetIn.rule
     , NoMissingTypeExpose.rule
-    , NoSimpleLetBody.rule
+    , NoModuleOnExposedNames.rule
     , NoPrematureLetComputation.rule
-    , NoUnused.CustomTypeConstructors.rule []
+    , NoRecursiveUpdate.rule
+    , NoRedundantlyQualifiedType.rule
+    , NoSimpleLetBody.rule
+    , NoSinglePatternCase.rule NoSinglePatternCase.fixInArgument
+    , NoUnnecessaryTrailingUnderscore.rule
+    , NoUnoptimizedRecursion.rule (NoUnoptimizedRecursion.optOutWithComment "IGNORE TCO")
+    , NoUnsafeDivision.rule
+    , NoUnsafePorts.rule NoUnsafePorts.onlyIncomingPorts
     , NoUnused.CustomTypeConstructorArgs.rule
+    , NoUnused.CustomTypeConstructors.rule []
     , NoUnused.Dependencies.rule
     , NoUnused.Exports.rule
+
+    -- , NoUnused.Modules.rule
     , NoUnused.Parameters.rule
     , NoUnused.Patterns.rule
     , NoUnused.Variables.rule
+    , Review.Documentation.CodeSnippet.check
+    , ReviewPipelineStyles.rule pipelineConfig |> Rule.ignoreErrorsForDirectories [ "tests/" ]
     , Simplify.rule Simplify.defaults
+    , UseCamelCase.rule UseCamelCase.default
+    , Validate.Regexes.rule
+    , VariablesBetweenCaseOf.AccessInCases.forbid
+    ]
+
+
+pipelineConfig : List (ReviewPipelineStyles.PipelineRule ())
+pipelineConfig =
+    [ ReviewPipelineStyles.forbid ReviewPipelineStyles.leftPizzaPipelines
+        |> ReviewPipelineStyles.andTryToFixThemBy ReviewPipelineStyles.Fixes.convertingToParentheticalApplication
+        |> ReviewPipelineStyles.andCallThem "forbidden <| pipeline"
+    , ReviewPipelineStyles.forbid ReviewPipelineStyles.leftCompositionPipelines
+        |> ReviewPipelineStyles.andCallThem "forbidden << composition"
+    , ReviewPipelineStyles.forbid ReviewPipelineStyles.rightCompositionPipelines
+        |> ReviewPipelineStyles.andCallThem "forbidden >> composition"
     ]
