fa3998eb715a962ca4434bf2ab8f98205321ac7f
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-01-28 11:10:19 +0100

    Add rules for package

--- package/src/ReviewConfig.elm
+++ package/src/ReviewConfig.elm
@@ -11,20 +11,40 @@ when inside the directory containing this file.
 
 -}
 
-import Docs.NoMissing exposing (exposedModules, onlyExposed)
+import Derive
+import Docs.NoMissing
 import Docs.ReviewAtDocs
 import Docs.ReviewLinksAndSections
 import Docs.UpToDateReadmeLinks
+import EqualsCaseable
+import HtmlToElm
+import LimitAliasedRecordSize
+import NoBooleanCase
+import NoBrokenParserFunctions
+import NoCatchAllForSpecificRemainingPatterns
 import NoConfusingPrefixOperator
 import NoDebug.Log
 import NoDebug.TodoOrToString
+import NoDeprecated
+import NoDuplicatePorts
 import NoExposingEverything
 import NoImportingEverything
+import NoInternalImports
+import NoInvalidRGBValues
+import NoMissingSubscriptionsCall
 import NoMissingTypeAnnotation
 import NoMissingTypeAnnotationInLetIn
 import NoMissingTypeExpose
+import NoModuleOnExposedNames
 import NoPrematureLetComputation
+import NoRecursiveUpdate
+import NoRedundantlyQualifiedType
 import NoSimpleLetBody
+import NoSinglePatternCase
+import NoUnnecessaryTrailingUnderscore
+import NoUnoptimizedRecursion
+import NoUnsafeDivision
+import NoUnsafePorts
 import NoUnused.CustomTypeConstructorArgs
 import NoUnused.CustomTypeConstructors
 import NoUnused.Dependencies
@@ -32,36 +52,80 @@ import NoUnused.Exports
 import NoUnused.Parameters
 import NoUnused.Patterns
 import NoUnused.Variables
+import Review.Documentation.CodeSnippet
 import Review.Rule as Rule exposing (Rule)
+import ReviewPipelineStyles
+import ReviewPipelineStyles.Fixes
 import Simplify
+import UseCamelCase
+import Validate.Regexes
+import VariablesBetweenCaseOf.AccessInCases
 
 
 config : List Rule
 config =
     [ Docs.NoMissing.rule
-        { document = onlyExposed
-        , from = exposedModules
+        { document = Docs.NoMissing.onlyExposed
+        , from = Docs.NoMissing.exposedModules
         }
-    , Docs.ReviewLinksAndSections.rule
+    , Derive.rule True []
     , Docs.ReviewAtDocs.rule
+    , Docs.ReviewLinksAndSections.rule
     , Docs.UpToDateReadmeLinks.rule
+    , EqualsCaseable.forbid EqualsCaseable.Everywhere
+    , HtmlToElm.rule
+    , LimitAliasedRecordSize.rule (20 |> LimitAliasedRecordSize.maxRecordSize)
+    , NoBooleanCase.rule
+    , NoBrokenParserFunctions.rule
+    , NoCatchAllForSpecificRemainingPatterns.rule { onlyReportCatchAllIfEquivalentToSinglePattern = False }
     , NoConfusingPrefixOperator.rule
-    , NoDebug.Log.rule
-    , NoDebug.TodoOrToString.rule
-        |> Rule.ignoreErrorsForDirectories [ "tests/" ]
+    , NoDebug.Log.rule |> Rule.ignoreErrorsForDirectories [ "tests/" ]
+    , NoDebug.TodoOrToString.rule |> Rule.ignoreErrorsForDirectories [ "tests/" ]
+    , NoDeprecated.rule NoDeprecated.defaults
+    , NoDuplicatePorts.rule
     , NoExposingEverything.rule
     , NoImportingEverything.rule []
+    , NoInternalImports.rule []
+    , NoInvalidRGBValues.rule
+    , NoMissingSubscriptionsCall.rule
     , NoMissingTypeAnnotation.rule
     , NoMissingTypeAnnotationInLetIn.rule
     , NoMissingTypeExpose.rule
-    , NoSimpleLetBody.rule
+    , NoModuleOnExposedNames.rule
     , NoPrematureLetComputation.rule
-    , NoUnused.CustomTypeConstructors.rule []
+    , NoRecursiveUpdate.rule
+    , NoRedundantlyQualifiedType.rule
+    , NoSimpleLetBody.rule
+    , NoSinglePatternCase.rule NoSinglePatternCase.fixInArgument
+    , NoUnnecessaryTrailingUnderscore.rule
+    , NoUnoptimizedRecursion.rule (NoUnoptimizedRecursion.optOutWithComment "IGNORE TCO")
+    , NoUnsafeDivision.rule
+    , NoUnsafePorts.rule NoUnsafePorts.onlyIncomingPorts
     , NoUnused.CustomTypeConstructorArgs.rule
+    , NoUnused.CustomTypeConstructors.rule []
     , NoUnused.Dependencies.rule
     , NoUnused.Exports.rule
+
+    -- , NoUnused.Modules.rule
     , NoUnused.Parameters.rule
     , NoUnused.Patterns.rule
     , NoUnused.Variables.rule
+    , Review.Documentation.CodeSnippet.check
+    , ReviewPipelineStyles.rule pipelineConfig |> Rule.ignoreErrorsForDirectories [ "tests/" ]
     , Simplify.rule Simplify.defaults
+    , UseCamelCase.rule UseCamelCase.default
+    , Validate.Regexes.rule
+    , VariablesBetweenCaseOf.AccessInCases.forbid
+    ]
+
+
+pipelineConfig : List (ReviewPipelineStyles.PipelineRule ())
+pipelineConfig =
+    [ ReviewPipelineStyles.forbid ReviewPipelineStyles.leftPizzaPipelines
+        |> ReviewPipelineStyles.andTryToFixThemBy ReviewPipelineStyles.Fixes.convertingToParentheticalApplication
+        |> ReviewPipelineStyles.andCallThem "forbidden <| pipeline"
+    , ReviewPipelineStyles.forbid ReviewPipelineStyles.leftCompositionPipelines
+        |> ReviewPipelineStyles.andCallThem "forbidden << composition"
+    , ReviewPipelineStyles.forbid ReviewPipelineStyles.rightCompositionPipelines
+        |> ReviewPipelineStyles.andCallThem "forbidden >> composition"
     ]
